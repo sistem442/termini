87d7079d463233396341ef5d715d0521ed0ea9bf
boris <boris.klinko@gmail.com>
2014-07-03 13:14:34 +0200

    finished work on entity

--- module/Application/src/Application/Entity/Event.php
+++ module/Application/src/Application/Entity/Event.php
@@ -4,18 +4,7 @@ use Doctrine\ORM\Mapping as ORM;
 /** @ORM\Entity */
 class Event
 {
-    // ...
-
-    /**
-     * @ORM\ManyToMany(targetEntity="Employee")
-     * @ORM\JoinTable(name="events_employees",
-     *      joinColumns={@ORM\JoinColumn(name="event_id", referencedColumnName="id")},
-     *      inverseJoinColumns={@ORM\JoinColumn(name="employee_id", referencedColumnName="id")}
-     *      )
-     **/
-    
-    private $events;
-    /**
+     /**
     * @ORM\Id
     * @ORM\GeneratedValue(strategy="AUTO")
     * @ORM\Column(type="integer")
@@ -24,8 +13,33 @@ class Event
     /**
     * @ORM\Column(type="string")
     */
-    
     protected $name_of_event;
+    /**
+    * @ORM\Column(type="date")
+    */
+    protected $date;
+    /**
+    * @ORM\Column(type="string")
+    * @ORM\Column(length=50)
+    */
+    protected $location;
+    /**
+    * @ORM\Column(type="time")
+    */
+    protected $time_of_event;
+    /**
+    * @ORM\Column(type="string")
+    * @ORM\Column(length=500)
+    */
+    protected $remark;
+    /**
+    * @ORM\Column(type="string")
+    * @ORM\Column(length=30)
+    */
+    protected $redaction;
+    
+    
+    
     public function setName_of_event($value)
         {
             $this->name_of_event = $value;
@@ -50,4 +64,34 @@ class Employee
     * @ORM\Column(type="integer")
     */
     protected $id;
+    /**
+    * @ORM\Column(type="string")
+    * @ORM\Column(length=30)
+    */
+    protected $name;
+}
+
+/** @ORM\Entity */
+class Work_schedule
+{
+    /**
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    * @ORM\Column(type="integer")
+    */
+    protected $id;
+    /**
+     * @ORM\ManyToOne(targetEntity="Work_schedule")
+     * @ORM\JoinColumn(name="event_id", referencedColumnName="id")
+     */
+    private $event;
+    /**
+     * @ORM\ManyToOne(targetEntity="Work_schedule")
+     * @ORM\JoinColumn(name="employee_id", referencedColumnName="id")
+     */
+    private $employee;
+    /**
+    * @ORM\Column(type="time")
+    */
+    protected $emp_start_time;
 }
